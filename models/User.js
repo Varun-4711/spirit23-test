37290dd2e3ce23d85ec6297bcdb76524800b112b
Mr-ut <106883528+Mr-ut@users.noreply.github.com>
2023-09-25 14:51:38 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,8 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     email: {
         type: String,
-        required: true,
-        unique: true
+        required: true
     },
     password: {
         type: String,
