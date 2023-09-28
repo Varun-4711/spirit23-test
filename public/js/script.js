15ca595eec08ea06d2ad4256562dc1d5d97488f4
Varun-4711 <119773328+Varun-4711@users.noreply.github.com>
2023-09-28 13:14:22 +0530

    news api-correction 3

--- public/js/script.js
+++ public/js/script.js
@@ -83,35 +83,35 @@ function startAnimation() {
 
 
 
-async function sports() {
-  const apikey ="e5a6e05216a74477b5ea26e1c01864ce";
-  const apiUrl ="https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=";
-  const response = await fetch(apiUrl + `${apikey}`);
-  var data = await response.json();
-  console.log(data);
-  document.querySelector(".h0").innerHTML = data.articles[1].title;
-  document.querySelector(".des0").innerHTML = data.articles[1].description;
-  const iconUrl = data.articles[1].urlToImage;
-  const imgElement = document.getElementById("img_style1");
-  imgElement.src = iconUrl;
+// async function sports() {
+//   const apikey ="e5a6e05216a74477b5ea26e1c01864ce";
+//   const apiUrl ="https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=";
+//   const response = await fetch(apiUrl + `${apikey}`);
+//   var data = await response.json();
+//   console.log(data);
+//   document.querySelector(".h0").innerHTML = data.articles[1].title;
+//   document.querySelector(".des0").innerHTML = data.articles[1].description;
+//   const iconUrl = data.articles[1].urlToImage;
+//   const imgElement = document.getElementById("img_style1");
+//   imgElement.src = iconUrl;
 
-  document.querySelector(".h1").innerHTML = data.articles[2].title;
-  document.querySelector(".des1").innerHTML = data.articles[2].description;
-  const iconUrl1 = data.articles[2].urlToImage;
-  const imgElement1 = document.getElementById("img_style2");
-  imgElement1.src = iconUrl1;
+//   document.querySelector(".h1").innerHTML = data.articles[2].title;
+//   document.querySelector(".des1").innerHTML = data.articles[2].description;
+//   const iconUrl1 = data.articles[2].urlToImage;
+//   const imgElement1 = document.getElementById("img_style2");
+//   imgElement1.src = iconUrl1;
 
-  document.querySelector(".h2").innerHTML = data.articles[3].title;
-  document.querySelector(".des2").innerHTML = data.articles[3].description;
-  const iconUrl2 = data.articles[3].urlToImage;
-  const imgElement2 = document.getElementById("img_style3");
-  imgElement2.src = iconUrl2;
+//   document.querySelector(".h2").innerHTML = data.articles[3].title;
+//   document.querySelector(".des2").innerHTML = data.articles[3].description;
+//   const iconUrl2 = data.articles[3].urlToImage;
+//   const imgElement2 = document.getElementById("img_style3");
+//   imgElement2.src = iconUrl2;
 
-  document.querySelector(".h3").innerHTML = data.articles[4].title;
-  document.querySelector(".des3").innerHTML = data.articles[4].description;
-  const iconUrl3 = data.articles[4].urlToImage;
-  const imgElement3 = document.getElementById("img_style4");
-  imgElement3.src = iconUrl3;
-}
-sports();
+//   document.querySelector(".h3").innerHTML = data.articles[4].title;
+//   document.querySelector(".des3").innerHTML = data.articles[4].description;
+//   const iconUrl3 = data.articles[4].urlToImage;
+//   const imgElement3 = document.getElementById("img_style4");
+//   imgElement3.src = iconUrl3;
+// }
+// sports();
 
