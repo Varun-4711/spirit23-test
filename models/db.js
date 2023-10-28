f3139f282237234327917a74b6e6a800151682b2
Varun-4711 <119773328+Varun-4711@users.noreply.github.com>
2023-10-28 16:47:47 +0530

    protected

--- models/db.js
+++ models/db.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 
 const mongoURL = process.env.DB_LINK;
 
-//`mongodb+srv://varunsaxenaiitg:KV2hUUlEv3tdM26v@cluster0.1tju1nl.mongodb.net/`;
-
 // const mongoURL =`mongodb+srv://rp8550495:vipZux-dogdo2-cyhxaz@cluster0.xk5bgmf.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.set("strictQuery", false);
