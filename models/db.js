84a243775b3c92acd061ad512ad6e3998bd3f59b
Varun-4711 <119773328+Varun-4711@users.noreply.github.com>
2023-10-28 16:13:57 +0530

    hidden

--- models/db.js
+++ models/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURL =`mongodb+srv://varunsaxenaiitg:KV2hUUlEv3tdM26v@cluster0.1tju1nl.mongodb.net/`;
+const mongoURL =process.env.DB_LINK;
+
 // const mongoURL =`mongodb+srv://rp8550495:vipZux-dogdo2-cyhxaz@cluster0.xk5bgmf.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.set("strictQuery", false);
